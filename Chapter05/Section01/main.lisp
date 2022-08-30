f7e718a8fc0b3023501cf96413ef9de3c42a48eb
ndaDayo <hayato711+nda@gmail.com>
2022-08-30 20:49:23 +0900

    add nodes

---
+++ Chapter05/Section01/main.lisp
@@ -0,0 +1,6 @@
+(defparameter *nodes* '((living-room (you are in the living room))
+                        (garden (you are in a beatiful garden))
+                        (attic (you are in the attic))
+                        ))
+
+
