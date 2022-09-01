dacf62e6f26f59cf0b188d1e1e14a21eadf3d6b9
ndaDayo <hayato711+nda@gmail.com>
2022-09-01 22:22:13 +0900

    オブジェクトをリストする

--- Chapter05/Section01/main.lisp
+++ Chapter05/Section01/main.lisp
@@ -7,3 +7,29 @@
   (cadr (assoc location nodes)))
  
 (print (decribe-location 'living-room *nodes*))
+
+(defparameter *edges* '((living-room (garden west door)
+                                     (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(print (describe-paths 'living-room *edges*))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-location* '((whiskey living-room)
+                                  (bucket living-room)
+                                  (frog garden)
+                                  (chain garden)))
+
+(defun object-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+             (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(print (object-at 'living-room *objects* *object-location*))
